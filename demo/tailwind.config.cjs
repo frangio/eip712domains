cacbbb42a39839116ab8208e9a3b5657155c2dfa
Francisco Giordano <fg@frang.io>
2023-11-03 19:39:21 +0000

    Revert "init shadcn-svelte"
    This reverts commit 95abf48a53b2069602f37c948324328800afa265.

---
+++ demo/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("tailwindcss").Config}*/
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [
+    require("@tailwindcss/forms"),
+  ],
+};
+
+module.exports = config;
