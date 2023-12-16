9a6b563f43490feb97d2a17d123c706cdd812f2d
Francisco Giordano <fg@frang.io>
2023-12-16 16:07:25 -0300

    Revert "inline layout"
    This reverts commit ab73e7dde5dcb9ebe08dd413234d99ca86549bbc.

--- demo/src/routes/+layout.svelte
+++ demo/src/routes/+layout.svelte
@@ -6,4 +6,21 @@
   inject({ mode: dev ? "development" : "production" });
 </script>
 
-<slot />
+<svelte:head>
+<title>EIP-712 Domain Helper</title>
+</svelte:head>
+
+<div class="max-w-lg space-y-8 p-2 pt-4 mx-auto">
+  <div class="space-y-2">
+    <h1 class="font-bold text-2xl">
+      <a href="/">EIP-712 Domain Helper</a>
+    </h1>
+
+    <div class="space-x-2 [&>a:hover]:underline">
+      <a href="/about" data-sveltekit-preload-data>About</a>
+      <a href="https://github.com/frangio/eip712domains">Source</a>
+    </div>
+  </div>
+
+  <slot />
+</div>
