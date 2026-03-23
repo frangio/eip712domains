01abef5be7ca7a145d3802e29a4599f7a5d831cc
Francisco Giordano <fg@frang.io>
2026-03-23 15:44:24 -0300

    fix build error

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
   resolve: {
-    preserveSymlinks: true,
     conditions: ["typescript"],
   },
   server: {
